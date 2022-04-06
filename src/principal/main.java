92b2f178fa1b252b2f8d5eb34685bad48395d1c7
Solid-Lizard <95529607+Solid-Lizard@users.noreply.github.com>
2022-04-06 10:42:24 +0200

    Añadimos el saludo al creador

--- src/principal/main.java
+++ src/principal/main.java
@@ -11,7 +11,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("¡Hola mundo!");	
+		System.out.println("¡Hola mundo!");
+		System.out.println("¡Hola Santiago!");
 
 	}
 
